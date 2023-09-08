e1523a7a8dbad435d50117077db99b50ef46181d
jaaviieerx <javiertjh@gmail.com>
2023-09-08 20:45:43 +0800

    cleanup

--- main.py
+++ main.py
@@ -3,64 +3,62 @@ import time
 import argparse
 from pathlib import Path
 
-# CLI arguments
-parser = argparse.ArgumentParser()
-parser.add_argument("input", type=argparse.FileType('r'))
-parser.add_argument("-o", "--outputDirectory", type=Path,
-                    help="path for pictures", default="output")
-parser.add_argument("-t", "--threads", type=int,
-                    help="no. of threads", default=8)
-parser.add_argument("-p", "--percentage", type=int,
-                    help="percentage less of highest clarity found as minimum clarity", default=5)
+def main():
+    # CLI arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("inputFile", type=argparse.FileType('r'))
+    parser.add_argument("-o", "--outputDirectory", type=Path,
+                        help="path for pictures", default="output")
+    parser.add_argument("-t", "--threads", type=int,
+                        help="no. of threads", default=8)
+    parser.add_argument("-p", "--percentage", type=int,
+                        help="percentage less of highest clarity found as minimum clarity", default=5)
+    p = parser.parse_args()
 
-p = parser.parse_args()
+    # calculate time start to process
+    start = time.time()
 
-# configs
-FIRST_FNO = 0
-LAST_FNO = 0
+    # open video file
+    video = cv2.VideoCapture(p.inputFile.name)
+    total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
 
-# open video file
-video = cv2.VideoCapture(p.input.name)
+    max_clarity = 0
+    processed_frames = []
 
-# get metadata
-fps = int(video.get(cv2.CAP_PROP_FPS))
-total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
-height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    # process clarity of every frame
+    for fno in range(0, total_frames):
+        video.set(cv2.CAP_PROP_POS_FRAMES, fno)
+        _, image = video.read()
+        # calculate clarity (the higher, the clearer)
+        clarity = cv2.Laplacian(image, cv2.CV_64F).var()
+        frame = {'fno': fno, 'clarity': clarity, 'image': image}
+        processed_frames.append(frame)
+        # get highest clarity
+        if (clarity > max_clarity):
+            max_clarity = clarity
 
-# calculate time start to process
-start = time.time()
+    # extract multiple frames of similar clarity
+    allowance_rate = (100 - p.percentage) / 100
+    min_clarity = max_clarity * allowance_rate
+    for frame in processed_frames:
+        if (frame['clarity'] > min_clarity):
+            cv2.imwrite(
+                f"{p.outputDirectory}/{frame['fno']}.png", frame['image'])
 
-max_clarity = 0
-all_fno_frame = {}
+    # calculate time end to process
+    end = time.time()
 
-# TODO multithreaded
-# process clarity of every frame
-LAST_FNO = total_frames if LAST_FNO <= FIRST_FNO else LAST_FNO
-for fno in range(FIRST_FNO, LAST_FNO):
-    video.set(cv2.CAP_PROP_POS_FRAMES, fno)
-    _, image = video.read()
-    # calculate clarity (the higher, the clearer)
-    clarity = cv2.Laplacian(image, cv2.CV_64F).var()
-    frame = {'clarity': clarity, 'image': image}
-    all_fno_frame[fno] = frame
-    # get highest clarity
-    if (clarity > max_clarity):
-        max_clarity = clarity
+    fps = int(video.get(cv2.CAP_PROP_FPS))
+    width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-# extract multiple frames of similar clarity
-allowance_rate = (100 - p.percentage) / 100
-min_clarity = max_clarity * allowance_rate
-for fno, frame in all_fno_frame.items():
-    if (frame['clarity'] > min_clarity):
-        cv2.imwrite(f'{p.outputDirectory}/{fno}.png', frame['image'])
+    # log data
+    print(f"Time elapsed: {round(end - start, 2)}s")
+    print("Total frames:", total_frames)
+    print(f"Resolution: {width}x{height}")
+    print(f"Clarity: {round(min_clarity, 2)} to {round(max_clarity, 2)}")
+    print("FPS:", fps)
 
-# calculate time end to process
-end = time.time()
 
-# log data
-print(f"Time elapsed: {round(end - start, 2)}s")
-print("Total frames:", total_frames)
-print(f"Resolution: {width}x{height}")
-print(f"Clarity: {round(min_clarity, 2)} to {max_clarity}")
-print("FPS:", fps)
+if __name__ == '__main__':
+    main()
